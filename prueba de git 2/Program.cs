058b963329b58290ba80868dc2ff36be6cdd4e2e
CESAR ALESSANDRO MEJIA PEREZ <camejiap@correo.url.edu.gt>
2022-09-01 20:56:17 -0600

    actualización de pruebas 1

--- prueba de git 2/Program.cs
+++ prueba de git 2/Program.cs
@@ -10,6 +10,8 @@ namespace prueba_de_git_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo, esta es una prueba de git");
+            Console.ReadKey();
         }
     }
 }
